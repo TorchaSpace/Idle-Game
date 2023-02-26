1e352ac57ea312642df4d79bf07433c642bf4dd3
TorchaSpace <ali.emre.celix@gmail.com>
2023-02-26 16:20:28 +0300

    Clicker

---
+++ idle/Assets/Scripts/GainCoinButton.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GainCoinButton : MonoBehaviour
+{
+    public Text coinText;
+
+    public double coins;
+
+    private void Start()
+    {
+        coins = 0;
+    }
+
+    private void Update()
+    {
+        coinText.text = "Coins: " + coins; 
+    }
+
+    public void Click()
+    {
+        coins += 1;
+    }
+}
